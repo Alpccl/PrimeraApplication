cdb9f8a893e4e43d8af8e7c24d8e60602fc971bd
Alpccl <aledleonv@gmail.com>
2020-12-21 22:13:08 -0300

    Cambios realizados que agregan funcionalidad de suma a la app

--- app/src/main/java/com/example/primera/MainActivity.java
+++ app/src/main/java/com/example/primera/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.primera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText1 = findViewById(R.id.editTextNumber);
+        EditText editText2 = findViewById(R.id.editTextNumber2);
+        TextView mTextView = findViewById(R.id.TextView);
+        Button button = findViewById(R.id.button);
+
+
+
+        // el codigo fue autogenerado
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String num1 = editText1.getText().toString();
+                String num2 = editText2.getText().toString();
+                int result = Integer.parseInt(num1)+ Integer.parseInt(num2) ;
+                mTextView.setText("El resultado es:  " + result);
+
+            }
+        });
+
+
     }
+
 }
